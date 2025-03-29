873b5e559bde464ad38444dd9d6843f63c1690bf
itsmeow <itsmeow@itsmeow.dev>
2025-03-29 00:49:36 -0500

    restore balance to the universe (highp floats)

--- src/glsl/space_layer_bounded_fragment.glsl
+++ src/glsl/space_layer_bounded_fragment.glsl
@@ -1,4 +1,8 @@
-precision mediump float;
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+  precision highp float;
+#else
+  precision mediump float;
+#endif
 
 uniform float   u_offset;
 uniform vec2    u_resolution;
